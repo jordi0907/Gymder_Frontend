711682dc6974a59bf7ca9b2a6f84ebd6c223e8e0
Miguel Angel Rincon <miguel.angel.rincon@estudiantat.upc.edu>
2021-05-05 12:12:45 +0200

    Mejora con cards en Reservar
    He añadido card, así se ajusta mejor al teléfono.

--- gymderapp/src/app/pages/tab3/reservar/reservar.page.ts
+++ gymderapp/src/app/pages/tab3/reservar/reservar.page.ts
@@ -1,6 +1,9 @@
+import { Usuario } from './../../../interfaces/interfaces';
+import { UsuarioService } from './../../../services/usuario.service';
 import { ReservarService } from 'src/app/services/reservar.service';
 import { Component, OnInit } from '@angular/core';
 import {Sala} from 'src/app/interfaces/interfaces'
+
 
 @Component({
   selector: 'app-reservar',
@@ -9,9 +12,11 @@ import {Sala} from 'src/app/interfaces/interfaces'
 })
 export class ReservarPage implements OnInit {
   salas :Sala[];
-  constructor(private reservarService: ReservarService) { }
+  usuario: Usuario ={};
+  constructor(private reservarService: ReservarService, private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.usuarioService.getUsuario();
     this.salas = this.reservarService.getSalas();
   }
 
