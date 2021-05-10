e4d415c4cfeb0f972a4463111931378f732440cf
Kevin Alcalde <kevin.javier.alcalde@estudiantat.upc.edu>
2021-05-10 10:16:24 +0200

    Revision Toni

--- gymderapp/src/app/pages/tab3/chat/chat.page.ts
+++ gymderapp/src/app/pages/tab3/chat/chat.page.ts
@@ -1,3 +1,4 @@
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { Usuario } from 'src/app/interfaces/interfaces';
@@ -12,29 +13,55 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 
 export class ChatPage implements OnInit {
 
-  usuario: Usuario = {};
 
+
+  usuario: Usuario = {};
+  usuario1 = {};
+  listaUser: Map<String,any> = new Map()
+  llave: String;
   constructor(
     private socket:Socket,
     private usuarioService: UsuarioService
     ) 
   
   { 
-    console.log(socket);
+    //console.log(socket);
     this.usuario = usuarioService.getUsuario(); 
-    let messages= {}
+    console.log ('', this.usuario);
+    this.llave = this.usuario.username;
+    console.log('llave : ',this.llave)
     this.socket.emit('connection')
-    this.socket.emit('new-message', this.usuario)
-    
-    this.socket.on('messages', function (data) {
-      messages = messages + data;
-      console.log('frontend'+messages);
-      
+    this.socket.emit('me-conecto', this.usuario);
 
-    });
+    this.socket.on('listausuarios', function(data){
+      this.listaUser = data;
+      console.log('la lista',this.listaUser);
+      
+       
+      
+      
+    
+    
+      
+    })
+  
+    console.log('llave2 :', this.llave)
+    this.usuario1 = this.listaUser[0]
+    console.log('hola1',this.usuario1);
+    console.log('prueba3',this.listaUser[1])
+
+   
+
+   
+    
   }
 
   ngOnInit() {
+
+    this.listaUser 
+
+    
+    
   }
 
 }
